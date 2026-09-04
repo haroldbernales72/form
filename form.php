0838b1f4e4c3b41fd033aac609d5ffb6d491eec6
Harold D. Bernales <haroldbernales72@gmail.com>
2026-09-04 19:45:55 +0800

    first commit

---
+++ form.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Page</title>
+    <link rel="stylesheet" href="reg.css">
+</head>
+<body>
+    <div class="container" >
+        <h1 style="text-align: center;">Register</h1>
+        <form id="registrationForm" action="index.php" method="POST">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+
+            <button type="submit" style="margin-left: 125px; height: 40px; width: 20%;">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
+
