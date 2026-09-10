8a03e5ec4ee1df7c7132a5df1701d470f96666c0
Harold D. Bernales <haroldbernales72@gmail.com>
2026-09-10 19:53:32 +0800

    Update title and heading from "Registration" to "Login" in form.php

--- form.php
+++ form.php
@@ -2,12 +2,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration Page</title>
+    <title>Login Page</title>
     <link rel="stylesheet" href="reg.css">
 </head>
 <body>
     <div class="container" >
-        <h1 style="text-align: center;">Register</h1>
+        <h1 style="text-align: center;">Login</h1>
         <form id="registrationForm" action="index.php" method="POST">
             <label for="username">Username:</label>
             <input type="text" id="username" name="username" required>
@@ -15,7 +15,7 @@
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required>
 
-            <button type="submit" style="margin-left: 125px; height: 40px; width: 20%;">Login</button>
+            <button type="Login" style="margin-left: 125px; height: 40px; width: 20%;">Login</button>
         </form>
     </div>
 </body>
